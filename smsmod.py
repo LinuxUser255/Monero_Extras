d7849a1180836059a2e684f6b96bfb985e17ecee
Chris <atnl@protonmail.com>
2022-04-14 08:57:50 -0400

    Create smsmod.py

---
+++ smsmod.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import requests
+import sys
+import recipient
+import xmrlert
+import urllib
+import urllib2
+
+# Under construction
+
+def SMS():
+  pass
+
+    
+
+
+
+sms()
